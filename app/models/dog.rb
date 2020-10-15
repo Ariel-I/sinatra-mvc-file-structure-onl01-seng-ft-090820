2b511e63604073f3df359eefa3d611497d127b9b
Ariel I <arielibarra39@gmail.com>
2020-10-15 03:30:15 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,18 +1,4 @@
 class Dog 
   
-  attr_accessor :name, :breed, :age 
-  
-  @@all = []
-  
-  def intialize
-    @name = name 
-    @breed = breed
-    @age = age
-    @@all << self 
-  end 
-  
-  def self.all
-    @@all 
-  end 
-  
+
 end 
